63af585cfc488df975da2b4e7bf083e99fa82df2
Tihici <tihamer.sebok@gmail.com>
2020-05-18 09:46:28 +0300

    vege

--- src/counter-button.js
+++ src/counter-button.js
@@ -1,3 +1,27 @@
-/**
- * Created by User on 5/10/2020.
- */
+Vue.component("counter-button", {
+
+    props: [ 'theme','offset'],
+
+    data: function(){
+        return{
+            counter:0
+        }
+
+    },
+
+    template: '<button @click="Increase()"  v-bind:class="classes">' +"{{counterPlusOffset}}</button>",
+
+    methods:{
+        Increase(){
+            this.counter++;
+        }
+    },
+    computed: {
+counterPlusOffset(){
+    return this.counter + +this.offset;
+},
+        classes(){
+    return ['btn', 'btn-' + this.theme];
+        }
+    }
+});
